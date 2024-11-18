7d00af2d8c697d2ecdf18c907948e8624c8f8ff4
BautistaSantosTapia <bautisantostapia04@gmail.com>
2024-11-18 21:49:32 +0000

    feat: Punto_A hecho

---
+++ RTA_Examen_20241118/Punto_A.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo pvcreate /dev/sdc
+sudo pvcreate /dev/sdd
+sudo vgcreate vg_datos /dev/sdc
+sudo vgcreate vg_tmp /dev/sdd
+sudo lvcreate -L 5M -n lv_docker vg_datos
+sudo lvcreate -L 1.5G -n lv_workareas vg_datos
+sudo lvcreate -L 512M -n lv_swap vg_tmp
+sudo mkfs -t ext4 /dev/mapper/vg_datos-lv_docker
+sudo mkfs -t ext4 /dev/mapper/vg_datos-lv_workareas
+sudo mkswap /dev/mapper/vg_tmp-lv_swap                                           
+sudo apt update
+sudo apt install apache2
+sudo mount /dev/mapper/vg_datos-lv_docker /var/lib/docker
+sudo mkdir -p /work
+sudo mount /dev/mapper/vg_datos-lv_workareas /work
+sudo swapon /dev/mapper/vg_tmp-lv_swap
+
